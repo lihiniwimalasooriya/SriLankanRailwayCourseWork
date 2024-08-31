70463a8203faedf064df581e719a18da32e1a3bf
lihiniwimalasooriya <lswimalasooriya@gmail.com>
2024-08-31 20:05:02 +0530

    new change

---
+++ backend/routes/protectedRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// Example of a protected route
+router.get("/protected-data", authMiddleware, (req, res) => {
+  res.json({
+    message: "This is protected data, only accessible with a valid token.",
+  });
+});
+
+module.exports = router;
